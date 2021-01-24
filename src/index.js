443a78555d3bb45fb01b00464924714a1228f681
Ankita Nigam <ankitanigam51@gmail.com>
2021-01-24 13:22:43 +0530

    Checking

--- src/index.js
+++ src/index.js
@@ -4,29 +4,38 @@ const bodyParser = require("body-parser");
 const posts = require('./initialData');
 const port = 3000
 app.use(express.urlencoded());
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(bodyParser.json())
 // your code goes here 
+
 let numOfApiCalls = 0;
 let initialMax = null;
 
+
 app.get('/api/posts', (req,res) => {
     if(numOfApiCalls >= 5) {
-        res.status(429).send({message: "Exceed Number of API calls"});
         res.status(429).send({message: "Exceed Number of API Calls"});
         return;
     }
 
+
     const parsedMax = Number(req.query.max || 10);
     const max = parsedMax > 20 ? 10: parsedMax;
     let finalMAx = max;
+
     if(initialMax !== null) {
         finalMAx = Math.min(finalMAx,initialMax);
     }
+
     const topmax = posts.filter((value,idx) => idx < finalMAx);
     res.send(topmax);
+
     if(initialMax === null) {
         initialMax = max;
         numOfApiCalls++;
@@ -37,6 +46,9 @@ app.get('/api/posts', (req,res) => {
     } else {
         numOfApiCalls++;
     }
+
 });
+
 app.listen(port, () => console.log(`App listening on port ${port}!`))
+
 module.exports = app;
